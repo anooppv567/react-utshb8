0aaffb0e7d77cff2da341ff9ef484a4d6643a699
anooppv567 <anoopit2010@gmail.com>
2019-08-05 14:29:17 -0500

    1 file modified

--- component/counter.jsx
+++ component/counter.jsx
@@ -2,17 +2,40 @@ import React, { Component } from 'react';
 
 class Counter extends Component{
 state = {
-  count : 0
+  count : 0,
+  tags : ['tag1','tag2','tag3']
 };
+styles = {
+  fontSize:10,
+  fontWeight:'bold'
+}
+
+renderTags(){
+  if(this.state.tags.length ===0) return 'No tags are there';
+ return <ul>
+      {this.state.tags.map(tag =><li key ={tag}>{tag}</li>)}
+  </ul>
+}
+handleCounter = ()=>{
+  this.state.count = this.state.count +1;
+  this.setState({count:this.state.count});
+  //console.log(this.state.count);
+}
 render(){
+  
     return (
       <div>
       <div className='container'>
-      <span className = 'badge badge-primary m-2'>{this.formatCount()}</span>
-      <button className = 'btn btn-secondary btn-sm'>+</button>
-      </div>
+      <span style = {this.styles} className ={this.getBadgeClasses()}>{this.formatCount()}</span>
+      <button onClick = {this.handleCounter}className = 'btn btn-secondary btn-sm'>+</button>
+     </div>
       </div>
       )
+  }
+  getBadgeClasses(){
+    let badgeClasses = 'badge m-2 badge-';
+        badgeClasses += (this.state.count === 0) ? 'warning' : 'primary';
+     return badgeClasses;
   }
 
   formatCount(){
