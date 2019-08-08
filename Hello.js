d464138227499cfb6c5ceac1ffff4acbf4a19b0e
anooppv567 <anoopit2010@gmail.com>
2019-08-08 14:14:37 -0500

    2 files modified

--- Hello.js
+++ Hello.js
@@ -2,14 +2,51 @@ import React, { Component } from 'react';
 import NavBar from './component/navBar';
 import Counters from './component/counters';
 class Hello extends Component{
+state = {
+  counters :[
+    {id:1,value:4},
+    {id:2,value:3},
+    {id:3,value:0},
+    {id:4,value:0}
+         ]
+}
+handleReset = ()=>{
+  const counters = this.state.counters.filter(counter =>{
+    counter.value =0;
+    return counter;
+  });
+  this.setState({counters});
 
+}
+handleDelete = (counterId)=>{
+const filteredCounters = this.state.counters.filter( counter =>counter.id !== counterId);
+this.setState( {counters : filteredCounters});
+}
+handleCounter = (counter)=>{
+  const counters = [...this.state.counters];
+  const index = counters.indexOf(counter);
+  counters[index].value++;
+ this.setState({counters});
+}
+decrementCounter = (counter) =>{
+  const counters = [...this.state.counters];
+  const index = counters.indexOf(counter);
+  counters[index].value--;
+  this.setState({counters})
+
+}
 render(){
 
   return(
     <React.Fragment>
     <NavBar/>
     <main className='container'>
-     <Counters/>
+     <Counters  
+     counters = {this.state.counters}
+      onDelete = {this.handleDelete}
+     onIncrement = {this.handleCounter}
+     onDecrement = {this.decrementCounter}
+     onReset = {this.handleReset}></Counters>
     </main>
     </React.Fragment>
   )
